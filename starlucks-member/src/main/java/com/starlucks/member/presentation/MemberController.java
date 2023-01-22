85ca431866ba58c3809a0d08c4e5599c18f1a77d
gyhcom <gyeonho@gamil.com>
2023-01-23 08:07:34 +0900

    new feature 1

---
+++ starlucks-member/src/main/java/com/starlucks/member/presentation/MemberController.java
@@ -0,0 +1,15 @@
+package com.starlucks.member.presentation;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
+public class MemberController {
+
+    @GetMapping(name = "hihi")
+    public String hello() {
+        return "hello";
+    }
+}
