1f2a659e049b050fd125204a6310a2368899a390
gyhcom <gyeonho@gamil.com>
2023-01-31 06:13:09 +0900

    Member Create APi

---
+++ starlucks-member/src/main/java/com/starlucks/member/infrastructure/config/MemberConfig.java
@@ -0,0 +1,15 @@
+package com.starlucks.member.infrastructure.config;
+
+import com.starlucks.member.application.processor.MemberAddProcessor;
+import com.starlucks.member.domain.repository.MemberRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MemberConfig {
+
+    @Bean
+    public MemberAddProcessor memberAddProcessor(MemberRepository memberRepository) {
+        return new MemberAddProcessor(memberRepository);
+    }
+}
