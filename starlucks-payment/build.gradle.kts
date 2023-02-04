9efd572c6ecbece58a89d732cf24bc6a6e59322c
JisuNa <kuu2002@naver.com>
2023-02-04 21:51:39 +0900

    #11 Add Payment

--- starlucks-payment/build.gradle.kts
+++ starlucks-payment/build.gradle.kts
@@ -1,1 +1,15 @@
-dependencies {  }
+plugins {
+    id("org.springframework.boot") version "3.0.1"
+    id("io.spring.dependency-management") version "1.1.0"
+}
+
+dependencies {
+    implementation(project(path = ":starlucks-common", configuration = null))
+
+    implementation("org.springframework.boot:spring-boot-starter-web")
+}
+
+tasks {
+    jar { enabled = true }
+    bootJar { enabled = false }
+}
