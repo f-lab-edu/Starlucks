b9aa7928432e5cdb77cd8be3acbb8d1f398b54de
gyhcom <59407250+gyhcom@users.noreply.github.com>
2023-02-15 13:14:44 +0900

    Revert "Feat: Add Member signUp api (#16)"
    This reverts commit 97f77e4b877bb803d4561d566fba4a22f89d58e3.

--- starlucks-member/src/main/java/com/starlucks/member/domain/IdGenerator.java
+++
@@ -1,5 +0,0 @@
-package com.starlucks.member.domain;
-
-public interface IdGenerator {
-    long generate();
-}
--- starlucks-member/src/main/java/com/starlucks/member/domain/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.starlucks.member.domain.repository;
-
-import com.starlucks.member.domain.entity.Member;
-
-public interface MemberRepository {
-
-    Member save(Member member);
-}
